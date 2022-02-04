e4a4c3ad7400d37390c88926abeb9aa2d2bac7ee
ramazanks637 <ramazanks637@gmail.com>
2022-02-04 19:19:52 +0300

    güncelle

--- DersKodlari/OOPOverview/Constructor/Program.cs
+++ DersKodlari/OOPOverview/Constructor/Program.cs
@@ -7,16 +7,21 @@ namespace Constructor
         static void Main(string[] args)
         {
             Ekmek ekmek = new Ekmek();
-            
             Console.WriteLine(ekmek.Tur);
-
+            Console.WriteLine();
             Ekmek ekmek2 = new Ekmek(3);
-
+            Console.WriteLine(ekmek2.Adet);
+            Console.WriteLine(ekmek2.Tur);
+            Console.WriteLine();
             Ekmek ekmek3 = new Ekmek("Kepekli");
-
+            
             Ekmek ekmek4 = new Ekmek(2, "Trabzon");
             
-            RaporUretici uretici  = new RaporUretici()
+            
+
+
+
+            //RaporUretici uretici  = new RaporUretici()
 
         }
     }
